599608ef2d6099628fd9c1e2b8f76e75ac7cf0b9
mohit  Kumar Sharma <kaushikmohit54@hotmail.com>
2018-09-28 16:58:48 +0530

    2nd commit

---
+++ mercury_tours/src/main/java/mercury_tours/mercury_tours/App.java
@@ -0,0 +1,13 @@
+package mercury_tours.mercury_tours;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
