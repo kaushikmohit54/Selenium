45b074c3627d4260c63fde6876f8dff06f4de402
mohit  Kumar Sharma <kaushikmohit54@hotmail.com>
2018-10-01 16:03:52 +0530

    browser test

---
+++ mercury_tours/src/test/java/Login/BaseTest.java
@@ -0,0 +1,26 @@
+package Login;
+
+
+import org.testng.annotations.Test;
+
+public class BaseTest extends Configuration {
+	
+	
+	@Test
+	public void login() {
+		driver.get("http://google.com");
+		
+		
+	}
+	
+	
+	
+	
+	
+
+	
+		
+	
+	
+
+}
