356a07d44c683fb969d56c8d24a2f83ff0b11b01
mohit  Kumar Sharma <kaushikmohit54@hotmail.com>
2018-10-04 23:15:34 +0530

    with advance cucumber report

---
+++ CcumberAdvanceReport/src/main/java/CcumberAdvanceReport/CcumberAdvanceReport/App.java
@@ -0,0 +1,13 @@
+package CcumberAdvanceReport.CcumberAdvanceReport;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
